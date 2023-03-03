31cfc62952af8a7ec9da081668658df96cc4833b
HoldontoGod <simbeyemonica@gmail.com>
2023-03-03 14:46:21 -0800

    Regular_expressions

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Match hbtn [One or more occurences of 'b']
+puts ARGV[0].scan(/hbt+n/).join
---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# match a three lettered string That:
+# Begins with 'h', ends with 'n'
+puts ARGV[0].scan(/^h.n$/).join
